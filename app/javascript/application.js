fbf637ad324f018c10f9775605f89dd496513473
Mark Finlay <markfinlay@Marks-iMac.local>
2024-01-18 18:41:49 +0000

    updates carousel images

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -10,5 +10,30 @@ $(document).ready(function () {
     $('.navbar').addClass('sticky');
   });
 
+  // Carousel
+  var $item = $('.carousel-item');
+  var $wHeight = $(window).height();
+  $item.eq(0).addClass('active');
+  $item.height($wHeight);
+  $item.addClass('full-screen');
+
+  $('.carousel img').each(function () {
+    var $src = $(this).attr('src');
+    var $color = $(this).attr('data-color');
+    $(this).parent().css({
+      'background-image': 'url(' + $src + ')',
+      'background-color': $color
+    });
+    $(this).remove();
+  });
+
+  $(window).on('resize', function () {
+    $wHeight = $(window).height();
+    $item.height($wHeight);
+  });
+
+  $('.carousel').carousel({
+    interval: 3000  // Adjust the interval to your preference (in milliseconds)
+  });
   console.log('Hello World from Webpacker 3')
 });
