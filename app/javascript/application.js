eff6cc7575e41753ad1c403bb3945ed66f415c13
Mark Finlay <markfinlay@Marks-iMac.local>
2024-01-18 18:26:44 +0000

    updates events copy

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,7 @@
 import * as bootstrap from "bootstrap"
 
+console.log('hey!')
+
 // Famous v1.1
 $(document).ready(function () {
 
@@ -8,25 +10,5 @@ $(document).ready(function () {
     $('.navbar').addClass('sticky');
   });
 
-  // Carousel
-  var $item = $('.carousel-item');
-  var $wHeight = $(window).height();
-  $item.eq(0).addClass('active');
-  $item.height($wHeight);
-  $item.addClass('full-screen');
-
-  $('.carousel img').each(function () {
-    var $src = $(this).attr('src');
-    var $color = $(this).attr('data-color');
-    $(this).parent().css({
-      'background-image': 'url(' + $src + ')',
-      'background-color': $color
-    });
-    $(this).remove();
-  });
-
-  $(window).on('resize', function () {
-    $wHeight = $(window).height();
-    $item.height($wHeight);
-  });
+  console.log('Hello World from Webpacker 3')
 });
